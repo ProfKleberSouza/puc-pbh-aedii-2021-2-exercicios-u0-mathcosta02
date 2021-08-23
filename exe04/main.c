8997f07a8d4120dcf095b797ca353b5f768b7df2
Matheus Costa <89105275+mathcosta02@users.noreply.github.com>
2021-08-23 21:16:59 +0000

    Exe04 completo

--- exe04/main.c
+++ exe04/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
+void menor_maior(int a, int b, int c, int *men, int *mai){
+  *men = a;
+  *mai = b;
+
+  // VERIFICA O MENOR NUMERO
+  if(b < *men){
+    *men = b;
+  }
+  if(c < *men){
+    *men = c;
+  }
+
+  // VERIFICA O MAIOR NUMERO
+  if(a > *mai){
+    *mai = a;
+  }
+  if(c > *mai){
+    *mai = c;
+  }
+}
+
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+  int n1, n2, n3, menor, maior;
+
+  // ENTRADA DE DADOS
+  scanf("%d %d %d", &n1, &n2, &n3);
+
+  // CHAMA PROCEDIMENTO
+  menor_maior(n1, n2, n3, &menor, &maior);
+
+  // SAIDA
+  printf("MENOR = %d\n", menor);
+  printf("MAIOR = %d\n", maior);
+  return 0;
 }
