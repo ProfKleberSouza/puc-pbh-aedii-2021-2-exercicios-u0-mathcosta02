35a64ede848a29299ca9aee411b00917fbf8aab9
Matheus Costa <89105275+mathcosta02@users.noreply.github.com>
2021-08-23 21:58:10 +0000

    Exe05 completo

--- exe05/main.c
+++ exe05/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+  float n1, n2, n3, n4, media;
+
+  // ENTRADA DE DADOS
+  scanf("%f %f %f %f", &n1, &n2, &n3, &n4);
+
+  // CALCULA A MEDIA
+  media = (n1+n2+n3+n4)/4;
+
+  // SAIDA
+  if(media >= 6){
+    printf("NOTA = %0.1f (APROVADO)\n", media);
+  }
+  else{
+    printf("NOTA = %0.1f (REPROVADO)\n", media);
+  }
+
+  return 0;
 }
