9e01544202a922ca84e151f501e5fb601c98da84
Matheus Costa <89105275+mathcosta02@users.noreply.github.com>
2021-08-23 20:53:50 +0000

    Exe03 completo

--- exe03/main.c
+++ exe03/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+  int n;
+
+  // ENTRADA DE DADOS
+  scanf("%d", &n);
+
+  printf("NUMERO ");
+
+  // SE O NUMERO FOR 0
+  if(n == 0){
+    printf("NEUTRO");
+  }
+  
+  // SE O NUMERO NAO FOR 0
+  else{
+    // PAR OU IMPAR
+    if(n % 2 == 0){
+      printf("PAR ");
+    }
+    else{
+      printf("IMPAR ");
+    }
+
+    // NEGATIVO OU POSITIVO
+    if(n < 0){
+      printf("NEGATIVO");
+    }
+    else{
+      printf("POSITIVO");
+    }
+  }
+   
+  return 0;
 }
