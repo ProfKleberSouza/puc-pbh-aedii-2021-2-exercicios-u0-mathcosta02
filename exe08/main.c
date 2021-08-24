fdefbde0c08f498cbe20844d54d9d1cff89dec90
Matheus Costa <89105275+mathcosta02@users.noreply.github.com>
2021-08-24 18:46:54 +0000

    Exe08 completo

--- exe08/main.c
+++ exe08/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+  int n=50, i, x;
+  int vet[n];
+
+  // DETERMINANDO VALORES AO VETOR
+  for(i=0; i<n; i++){
+    vet[i] = 1;
+  }
+  i=0;
+
+  // ALTERAR OS VALORES DO VETOR ATE UM VALOR SER 0
+  do{
+    scanf("%d", &vet[i]);
+
+    if(vet[i] != 0){
+      i++;
+    }
+
+  }while(vet[i] != 0);
+  i=0;
+
+  // MOSTRANDO NA TELA SE O NUMERO É POSITIVO OU NEGATIVO
+  do{
+    if(vet[i] > 0){
+      printf("POSITIVO\n");
+    }
+    else{
+      printf("NEGATIVO\n");
+    }
+    i++;
+  }while(vet[i] != 0);
+
+  return 0;
 }
