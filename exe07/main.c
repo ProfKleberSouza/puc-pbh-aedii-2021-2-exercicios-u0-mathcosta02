609c6f506d44e0dba0e4440094f873eec277b44e
Matheus Costa <89105275+mathcosta02@users.noreply.github.com>
2021-08-24 15:23:37 +0000

    Exe07 completo

--- exe07/main.c
+++ exe07/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 
+void preenche_vet(int vet[], int n){
+  for(int i=0; i<n; i++){
+    scanf("%d", &vet[i]);
+  }
+}
+
+void menor_maior(int vet[], int n, int *men, int *mai){
+  *men = vet[0];
+  *mai = vet[0];
+
+  for(int i=1; i<n; i++){
+    if(vet[i] < *men){
+      *men = vet[i];
+    }
+
+    if(vet[i] > *mai){
+      *mai = vet[i];
+    }
+  }
+}
+
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+  int n, menor, maior;
+
+  // TAMANHO DO VETOR
+  scanf("%d", &n);
+
+  int vet[n];
+
+  // CHAMA PROCEDIMENTOS
+  preenche_vet(vet, n);
+  menor_maior(vet, n, &menor, &maior);
+
+
+  // SAIDAS
+  printf("MENOR = %d\n", menor);
+  printf("MAIOR = %d\n", maior);
+
+  return 0;
 }
