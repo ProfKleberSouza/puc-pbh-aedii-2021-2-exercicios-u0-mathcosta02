47356324e4857a9d6e7892ed9f169c17cbd78e94
Matheus Costa <89105275+mathcosta02@users.noreply.github.com>
2021-08-25 00:11:57 +0000

    Exe10 completo

--- exe10/main.c
+++ exe10/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void preenche_vet(int vet[]){
+  for(int i=0; i<10; i++){
+    scanf("%d", &vet[i]);
+  }
+}
+
+void inverte_vetor(int vet[]){
+  int aux, i=0, n=9;
+
+  while(i<n){
+    aux = vet[i];
+    vet[i] = vet[n];
+    vet[n] = aux;
+
+    i++;
+    n--;
+  }
+}
+
+void mostra_vet(int vet[]){
+  for(int i=0; i<10; i++){
+    printf("%d\n", vet[i]);
+  }
+}
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+  int *vet = (int*)malloc(10*sizeof(int));
+
+  // CHAMA PROCEDIMENTOS
+  preenche_vet(vet);
+  inverte_vetor(vet);
+  mostra_vet(vet);
+
+  return 0;
 }
