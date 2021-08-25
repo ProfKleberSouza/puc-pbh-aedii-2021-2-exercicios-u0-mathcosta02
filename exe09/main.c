2d0807b42f33dce2a44c19ca6bf43663c21c7f76
Matheus Costa <89105275+mathcosta02@users.noreply.github.com>
2021-08-25 15:02:03 +0000

    Exe09 completo

--- exe09/main.c
+++ exe09/main.c
@@ -13,7 +13,7 @@ void cadastra_Alunos(struct Aluno vet[], int n){
   }
 }
 
-float nota_Media(struct Aluno vet[], int n){
+void nota_Media(struct Aluno vet[], int n){
   float media=0, soma=0;
   
   for(int i=0; i<n; i++){
@@ -21,7 +21,7 @@ float nota_Media(struct Aluno vet[], int n){
   }
 
   media = soma/n;
-  return media;
+  printf("\nNOTA MEDIA = %0.1f\n", media);
 }
 
 void maiores3_Notas(struct Aluno vet[], int n, float *nota1, float *nota2, float *nota3){
@@ -49,28 +49,57 @@ void maiores3_Notas(struct Aluno vet[], int n, float *nota1, float *nota2, float
 }
 
 void ordem_Alfa(struct Aluno vet[], int n, float n1, float n2, float n3){
+  int x=0;
+  float aux;
+  char aux2[15];
+
+  for(int i=0; i<n; i++){
+    if(vet[i].nota == n1 || vet[i].nota == n2 || vet[i].nota == n3){
+      // TROCAR POSICAO DAS MAIORES NOTAS
+      aux = vet[i].nota;
+      vet[i].nota = vet[x].nota;
+      vet[x].nota = aux;
+      // COPIAR OS NOMES DAS MAIORES NOTAS PARA O VET NOME3
+      strcpy(vet[x].nome3, vet[i].nome);
+      x++;
+    }
+  }
+  aux=0;
+
+  // ORDEM ALFABETICA DAS 3 MAIORES NOTAS
+  for(int x=0; x<3; x++){
+    for(int y=x+1; y<3; y++){
+      if(strcmp(vet[x].nome3, vet[y].nome3) > 0){
+        strcpy(aux2, vet[x].nome3);
+        strcpy(vet[x].nome3, vet[y].nome3);
+        strcpy(vet[y].nome3, aux2);
+
+        aux = vet[x].nota;
+        vet[x].nota = vet[y].nota;
+        vet[y].nota = aux;
+      }
+    }
+  }
+
+  for(int x=0; x<3; x++){
+    printf("%s %0.1f\n", vet[x].nome3, vet[x].nota);
+  }
 }
 
 int main() {
   int n;
-  float media, n1, n2, n3;
+  float n1, n2, n3;
 
   // QUANTIDADE DE ALUNOS
   scanf("%d", &n);
 
   struct Aluno vet[n];
 
-  // CHAMANDO FUNCOES E PROCEDIMENTOS
+  // CHAMANDO PROCEDIMENTOS
   cadastra_Alunos(vet, n);
-  media = nota_Media(vet, n);
+  nota_Media(vet, n);
   maiores3_Notas(vet, n, &n1, &n2, &n3);
   ordem_Alfa(vet, n, n1, n2, n3);
-
-  // SAIDA
-  printf("\nNOTA MEDIA = %0.1f\n", media);
-  printf("NOTA 1 = %0.1f\n", n1);
-  printf("NOTA 2 = %0.1f\n", n2);
-  printf("NOTA 3 = %0.1f\n", n3);
 
   return 0;
 }
